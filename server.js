2e7a3398231e9ced78cff08c04ba1b4a8e40048f
Surya Singh <44600494+Surya5599@users.noreply.github.com>
2020-11-26 10:56:31 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+  GNU nano 2.9.3                                                                                                     server.js                                                                                                               
+
+var express = require('express');
+var app = express();
+var server = app.listen(process.env.PORT || 3000);
+app.use(express.static('public'));
+console.log("server is running");
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+app.get('/', function(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('running');
+});
+
+function newConnection(socket){
+
+  console.log('new Connection: ' + socket.id);
+
+  socket.on('phoneData', sendData);
+
+
+  function sendData(data){
+    //console.log("data:",data)
+    socket.emit("phoneInfo", data);
+    //socket.broadcast.to(data.room).emit("userJoined", newData);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
